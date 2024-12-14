1138bf993a7bb762cf7df287d060e65d53bca378
HAMZA <hamzajaa2017@gmail.com>
2024-12-14 17:18:29 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="HAMZA"
+
+ENTRYPOINT ["top", "-b"]
---
+++ src/main/java/com/example/testrenderdeploy/TestRenderDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.testrenderdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRenderDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRenderDeployApplication.class, args);
+	}
+
+}
